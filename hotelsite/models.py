989738642c95f74fa18fc4166950b31a92f11b1a
Ethan Welborn <welborn.ethan@gmail.com>
2014-04-15 01:22:31 -0600

    step_3

--- hotelsite/models.py
+++ hotelsite/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+# from mongoengine import *
 
 # Create your models here.
+# class Review(Document):
+# 	location 			= StringField(max_length=256)
+# 	description 	= StringField(max_length=1024)
+# 	title 				= StringField(max_length=256)
+# 	star_rating 	= IntField(default=0)
+# 	date					= DateTimeField()
+
+	# meta = {
+	# 	'indexes': [
+	# 		'location',
+	# 		'title',
+	# 		'star_rating'
+	# 	]
+	# }
--- hotelsite/views.py
+++ hotelsite/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+# from hotelsite.models import Review
+
+# import mongoengine
 
 # Create your views here.
 def home(request):
@@ -10,4 +13,8 @@ def about(request):
 
 
 def reviews(request):
+	# user = authenticate(username=username, password=password)
+	# assert isinstance(user, mongoengine.django.auth.User)
+
+	# reviews = Review.objects().all()
 	return render(request, "hotelsite/reviews.html")
